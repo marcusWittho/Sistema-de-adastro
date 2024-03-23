6bb29f8347aca68d3e8cc1d4ae6f4923dda05106
wittho <witthoeft.mcs@hotmail.com>
2024-03-23 10:54:05 -0300

    Adição do nome da classe principal no arquivo build.gradle

--- app/src/main/java/org/wittho/EventosApplication.java
+++ app/src/main/java/org/wittho/EventosApplication.java
@@ -7,10 +7,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class App {
+public class EventosApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(App.class, args);
+    SpringApplication.run(EventosApplication.class, args);
   }
 
 }
